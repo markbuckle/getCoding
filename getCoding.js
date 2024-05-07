2aa98789d447b24fb51c2260400626ec8ea66b3c
Mark Buckle <markbuckle92@gmail.com>
2024-05-07 13:48:37 -0230

    Initial commit

---
+++ getCoding.js
@@ -0,0 +1,19 @@
+getCoding.js
+// shopping cart:
+const cart = [
+{
+item: "Pizza",
+count: 2,
+price: 15,
+},
+{
+item: "Garlic Fingers",
+count: 1,
+price: 10,
+},
+];
+console.log(cart[0].price);
+
+let total = cart[0].price*cart[0].count;
+total = total + cart [1]/price * cart[1].count;
+console.log(total);
